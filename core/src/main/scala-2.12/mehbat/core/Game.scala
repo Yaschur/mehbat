b7af4d9350e4dcc40902404e6fd82785451cfe64
Igor Trishin <yaschur@gmail.com>
2017-01-27 13:33:36 +0300

    Add bouts-rimes play methods

--- core/src/main/scala-2.12/mehbat/core/Game.scala
+++ core/src/main/scala-2.12/mehbat/core/Game.scala
@@ -1,16 +1,14 @@
 package mehbat.core
 
-import mehbat.Participant
-
 trait Game {
-	private var participants: Vector[Participant] = Vector.empty
+	protected var participants: Vector[String] = Vector.empty
 
 	protected def prepareGameFor(userId: String)
 	protected def clearGameFor(userId: String)
 
 	private[mehbat] def addGamer(who: String, whom: String): Unit = {
 		if (!isInGame(whom)) {
-			participants = participants :+ new Participant(whom)
+			participants = participants :+ whom
 			prepareGameFor(whom)
 		}
 	}
@@ -21,5 +19,5 @@ trait Game {
 		}
 	}
 	private[mehbat] def isInGame(userId: String): Boolean =
-		participants.exists(_.userId == userId)
+		participants.contains(userId)
 }
--- core/src/main/scala-2.12/mehbat/games/BoutsRimes.scala
+++ core/src/main/scala-2.12/mehbat/games/BoutsRimes.scala
@@ -5,6 +5,34 @@ import mehbat.core.Game
 class BoutsRimes extends Game {
 	private var lines: Vector[Line] = Vector.empty
 
+	def currentPlayer: Option[String] = {
+		if (participants.isEmpty) None
+		else {
+			lazy val evenMode = lines.length % 2 == 0
+			lines match {
+				case Vector.empty => Some(participants.head)
+				case _ :+ Line(a, _) if evenMode => Some(a)
+				case _ =>
+					val ind = participants.indexWhere(_ == lines.last.author)
+					Some(participants((ind + 1) % participants.length))
+			}
+		}
+	}
+	private[mehbat] def addLine(author: String, text: String): Unit = {
+		assert(!author.isEmpty && !text.isEmpty)
+		if (currentPlayer.getOrElse("") == author)
+			lines :+ Line(author, text)
+	}
+	private[mehbat] def getLines(userId: String): List[Line] = {
+		assert(!userId.isEmpty)
+		if (currentPlayer.getOrElse("") == userId)
+			lines match {
+				case _ :+ l1 :+ l2 if l2.author == userId => List(l1, l2)
+				case _ => List(lines.last)
+			}
+		else Nil
+	}
+
 	override protected def prepareGameFor(userId: String): Unit = ()
 
 	override protected def clearGameFor(userId: String): Unit =

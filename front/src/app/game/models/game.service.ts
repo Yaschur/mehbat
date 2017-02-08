213d714af33694d53a11568ba4444f55c7570588
Igor Trishin <yaschur@gmail.com>
2017-02-08 17:08:15 +0300

    Add add-line service

--- front/src/app/game/models/game.service.ts
+++ front/src/app/game/models/game.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AuthHttp } from 'angular2-jwt';
+import { Headers } from '@angular/http';
 import { environment } from '../../../environments/environment';
 
 import { Observable } from 'rxjs/Observable';
@@ -10,11 +11,17 @@ import { Game } from './game.model';
 @Injectable()
 export class GameService {
 
+	private headers = new Headers({ 'Content-Type': 'application/json' });
+
 	constructor(private aHttp: AuthHttp) { }
 
 	getGame(): Observable<Game> {
 		return this.aHttp
 			.get(environment.api)
 			.map(resp => resp.json() as Game);
+	}
+	addLine(line: string) {
+		this.aHttp
+			.post(environment.api, JSON.stringify({ line: line }), { headers: this.headers });
 	}
 }
